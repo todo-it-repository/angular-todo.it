3ef82d24230642808c0edd691199c0580ec37035
lucas <lucaston2323@gmail.com>
2025-03-18 19:17:11 -0300

    feat: implementar configurações iniciais para o header component (mobile-first)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [HomeComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-todolist';
+    title = 'angular-todolist';
 }
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    imports: [CommonModule],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+    
+}
