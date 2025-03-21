e460ee6ef7ee22272e134f4c2e1e26b9547b7de0
lucas <lucaston2323@gmail.com>
2025-03-21 18:02:50 -0300

    feat: implementar formulário de login

---
+++ src/app/components/input-form/input-form.component.ts
@@ -0,0 +1,50 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+
+type InputTypes = 'text' | 'password' | 'username';
+
+@Component({
+    selector: 'app-input-form',
+    imports: [
+        ReactiveFormsModule
+    ],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => InputFormComponent),
+            multi: true
+        }
+    ],
+    templateUrl: './input-form.component.html',
+    styleUrl: './input-form.component.css'
+})
+export class InputFormComponent implements ControlValueAccessor {
+    @Input() type: InputTypes = 'text';
+    @Input() placeholder: string = '';
+    @Input() label: string = '';
+    @Input() inputName: string = '';
+
+    value: string = '';
+    onChange: any = () => {};
+    onTouched: any = () => {};
+
+    onInput(e: Event) {
+        const value = (e.target as HTMLInputElement).value;
+        this.onChange(value);
+    }
+
+    writeValue(value: any): void {
+        this.value = value;
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    setDisabledState(isDisabled: boolean): void {
+    }
+}
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { DefaultLoginComponent } from '../../../components/default-login/default-login.component';
+import { InputFormComponent } from "../../../components/input-form/input-form.component";
+
 
 @Component({
-  selector: 'app-login',
-  imports: [ DefaultLoginComponent ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+    selector: 'app-login',
+    standalone: true,
+    imports: [
+    DefaultLoginComponent,
+    ReactiveFormsModule,
+    InputFormComponent
+],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    loginForm!: FormGroup;
 
+    constructor() {
+        this.loginForm = new FormGroup({
+            username: new FormControl('', [Validators.required, Validators.email]),
+            password: new FormControl('', [Validators.required, Validators.minLength(8)])
+        });
+    }
 }
