bfffc4fc5031de5a55fdbd5722aa03dc4229fe69
lucas <lucaston2323@gmail.com>
2025-05-05 18:38:54 -0300

    styles: adicionar responsividade para telas maiores

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonRoundComponent } from "../button-round/button-round.component";
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
-    imports: [CommonModule],
+    imports: [CommonModule, ButtonRoundComponent],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
 export class HeaderComponent {
     @Input() title: string = '';
     @Input() placeholder: string = '';
+    @Output() searchEvent = new EventEmitter<string>();
     isFocused: boolean = false;
+    searchContent: string = '';
+
+    constructor(
+        private router: Router
+    ) {
+    }
 
     search() {
-        console.log("pesquisado");
+        this.searchEvent.emit(this.searchContent);
     }
+
     onFocus() {
         this.isFocused = true;
     }
@@ -22,4 +32,9 @@ export class HeaderComponent {
     onBlur() {
         this.isFocused = false;
     }
+
+    navigate() {
+        this.router.navigate(['login']);
+    }
+
 }
